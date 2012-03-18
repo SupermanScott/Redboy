17d59b82bb58f51a78975b3ee04ff4182dd42116
Scott Reynolds <scott@scottreynolds.us>
2012-03-17 18:21:03 -0700

    Create a stack view

--- redboy/view.py
+++ redboy/view.py
@@ -8,8 +8,8 @@
 from redboy import get_pool
 from redboy.record import Record
 
-class QueueView(object):
-    """A QueueView is a set of Records ordered by when they were appended"""
+class View(object):
+    """A View is a set of Records. The how of the ordering is determined by Subclasses"""
     def __init__(self, view_key, record_class):
         """view_key is the redboy.key.Key for the set of records and
         record_class is the Record implementation."""
@@ -18,9 +18,7 @@ class QueueView(object):
 
     def append(self, record, new_record):
         """Add the Record to the View"""
-        # Save the records, non-prefix version.
-        if new_record:
-            get_pool(self.key.pool_name).rpush(str(self.key), record.key.key)
+        raise NotImplemented("Use a Subclass to append to the View")
 
     def __iter__(self):
         connection_pool = get_pool(self.key.pool_name)
@@ -37,3 +35,17 @@ class QueueView(object):
 
     def __len__(self):
         return get_pool(self.key.pool_name).llen(str(self.key))
+
+class Stack(View):
+    """A Stack is a set of records that is First In Last Out"""
+    def append(self, record, new_record):
+        if new_record:
+            get_pool(self.key.pool_name).lpush(str(self.key), record.key.key)
+
+class Queue(View):
+    """A Queue is a set of Records ordered by when they were appended"""
+    def append(self, record, new_record):
+        """Add the Record to the View"""
+        # Save the records, non-prefix version.
+        if new_record:
+            get_pool(self.key.pool_name).rpush(str(self.key), record.key.key)
