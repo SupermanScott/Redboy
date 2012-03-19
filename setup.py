f65060fe50d9c32e1aa0bd9072868673dcb28d76
Scott Reynolds <scott@scottreynolds.us>
2012-03-18 17:52:53 -0700

    add a few more nicities to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ import glob
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(name="Redboy",
       version='0.1.0',
       description="Object non-relational manager for Redis",
@@ -24,4 +27,10 @@ setup(name="Redboy",
       keywords="database cassandra",
       install_requires=['redis'],
       zip_safe=False,
-      tests_require=['nose', 'mock'])
+      tests_require=['nose', 'mock'],
+      long_description=read('README'),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+        ])
